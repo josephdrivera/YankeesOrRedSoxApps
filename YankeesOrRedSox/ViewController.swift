a1c8838d7dca0549517980525f6faa9729e7e36b
Joseph D Rivera <josephdriveramm@MassMutuals-MacBook-Pro.local>
2015-11-08 09:43:09 -0500

    added files for YankeesOrRedSox

--- YankeesOrRedSox/ViewController.swift
+++ YankeesOrRedSox/ViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+   
+    @IBOutlet weak var YankeesLogo: UIImageView!
+    @IBOutlet weak var RedSoxLogo: UIImageView!
+    @IBOutlet weak var YankeesBN: UIButton!
+    @IBOutlet weak var RedSoxBN: UIButton!
+    @IBOutlet weak var ResetBN: UIButton!
+    @IBOutlet weak var YankeesTitle: UITextField!
+    
+    @IBOutlet weak var RedSoxTitle: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +29,26 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func ShowYanleesLogo(sender: AnyObject) {
+        RedSoxBN.hidden = true
+        RedSoxLogo.hidden = true
+        YankeesBN.hidden = false
+    }
+    @IBAction func ShowRedSoxLogo(sender: AnyObject) {
+        YankeesBN.hidden = true
+        YankeesLogo.hidden = true
+        RedSoxBN.hidden = false
+    }
+    @IBAction func ResetBN(sender: AnyObject) {
+        RedSoxBN.hidden = false
+        RedSoxLogo.hidden = false
+        YankeesLogo.hidden = false
+        YankeesBN.hidden = false
+        
+        
+        
+        
+    }
 
 }
 
